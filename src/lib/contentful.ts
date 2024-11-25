3406d8124964648d158b0bcf1c442c4c12a24688
Tim Hilton <tim@timhilton.net>
2024-11-24 23:03:18 -0800

    fix tokens

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -39,9 +39,9 @@ export interface BlogItem {
 }
 
 export const contentfulClient = contentful.createClient({
-  space: process.env.VITE_CONTENTFUL_SPACE_ID!,
+  space: process.env.CONTENTFUL_SPACE_ID!,
   accessToken: process.env.NODE_ENV === 'development'
-    ? process.env.VITE_CONTENTFUL_PREVIEW_TOKEN!
-    : process.env.VITE_CONTENTFUL_DELIVERY_TOKEN!,
+    ? process.env.CONTENTFUL_PREVIEW_TOKEN!
+    : process.env.CONTENTFUL_DELIVERY_TOKEN!,
   host: process.env.NODE_ENV === 'development' ? "preview.contentful.com" : "cdn.contentful.com",
 });
