93de3f7566004c25a5b708daf169f98df8c5906c
Tim Hilton <tim@timhilton.net>
2024-11-24 23:17:07 -0800

    add adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: netlify(),
   integrations: [preact(), react()],
   prefetch: true
 });
