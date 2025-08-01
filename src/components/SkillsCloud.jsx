e0681ebc55114c21b896b466c3441706de1327f7
Tim Hilton <tim@timhilton.net>
2025-08-01 08:03:37 -0700

    (style): wrap canvas to have set width and height to stop thrashing

--- src/components/SkillsCloud.jsx
+++ src/components/SkillsCloud.jsx
@@ -22,6 +22,7 @@ export default function SkillsCloud() {
     const total = entries.length;
 
     return (
+        <div style={{ width: '100%', height: '630px', position: 'relative' }}>
         <Canvas camera={{ position: [0, 0, 8], fov: 50 }}>
             <ambientLight intensity={0.6} />
             <directionalLight position={[5, 5, 5]} intensity={1} />
@@ -39,5 +40,6 @@ export default function SkillsCloud() {
                 />
             ))}
         </Canvas>
+        </div>
     );
 }
