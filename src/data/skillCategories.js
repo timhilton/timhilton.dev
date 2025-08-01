2e344f9c63a1cb4c5f1dab4b47edcdfad2495a39
Tim Hilton <tim@timhilton.net>
2025-08-01 00:48:40 -0700

    (code): adds jsons

---
+++ src/data/skillCategories.js
@@ -0,0 +1,11 @@
+// src/data/skillCategories.js
+export const categoryColors = {
+    frontend: '#4FC3F7',
+    backend: '#81C784',
+    infrastructure: '#FFD54F',
+    mobile: '#BA68C8',
+    design: '#FF8A65',
+    cms: '#9575CD',
+    tools: '#E57373',
+    other: '#B0BEC5'
+};
